9e4f5869a65b8cde559ccb8b5652a1895c6116c0
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2022-03-01 20:56:01 -0400

    feat: webpack enviroment
    - add: config enviroment to use webpack
    - refactor: %PUBLIC% chante to relative path

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          }
+        ]
+      },
+      {
+        test: /\.(css|scss)?$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -20,6 +54,22 @@ module.exports = {
           },
         ],
       },
-    ],
+    ]
   },
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+      favicon: './public/favicon.ico'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  devServer: {
+    static:{
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+  },
+}
